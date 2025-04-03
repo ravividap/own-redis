cc1cc2defbfa892ea7768e79db5287dc6d4e85cf
Ravi Vidap <ravividap@ymail.com>
2025-04-03 21:54:13 +0530

    replid

--- src/InfoCommand.cs
+++ src/InfoCommand.cs
@@ -1,12 +1,28 @@
-﻿namespace codecrafters_redis.src
+﻿using System.Text;
+
+namespace codecrafters_redis.src
 {
     public class InfoCommand(bool isSlave) : IRedisCommand
     {
         public string Execute(string[] commandParts)
         {
-            var role = "role:" + (isSlave ? "slave" : "master");
+            var role = isSlave ? "slave" : "master";
+            var info = new Dictionary<string, string>();
 
-            return $"${role.Length}\r\n{role}\r\n";
+            info.Add("role", role);
+            info.Add("master_replid", "8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb");
+            info.Add("master_repl_offset", "0");
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"*{info.Count}\r\n");
+
+            foreach (var kv in info)
+            {
+                var kvString = $"{kv.Key}:{kv.Value}";
+                sb.Append($"${kvString.Length}\r\n{kvString}\r\n");
+            }
+
+            return sb.ToString();
         }
     }
 }
