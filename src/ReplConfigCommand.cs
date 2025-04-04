c526757cf457c83c6c6ba79294b1e4cd946fa13f
Ravi Vidap <ravividap@ymail.com>
2025-04-04 18:35:19 +0530

    reply to replconf

---
+++ src/ReplConfigCommand.cs
@@ -0,0 +1,10 @@
+﻿namespace codecrafters_redis.src
+{
+    public class ReplConfigCommand : IRedisCommand
+    {
+        public string Execute(string[] commandParts)
+        {
+            return "+OK\r\n";
+        }
+    }
+}
