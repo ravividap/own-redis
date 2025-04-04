514afe769df6ecd253f962cb6efb953edc19908d
Ravi Vidap <ravividap@ymail.com>
2025-04-04 18:46:54 +0530

    reply to psync

---
+++ src/PsyncCommand.cs
@@ -0,0 +1,12 @@
+﻿namespace codecrafters_redis.src
+{
+    public class PsyncCommand(string replicationId, int offset) : IRedisCommand
+    {
+        public string Execute(string[] commandParts)
+        {
+            Console.WriteLine();
+
+            return $"+FULLRESYNC {replicationId} {offset}\r\n";
+        }
+    }
+}
