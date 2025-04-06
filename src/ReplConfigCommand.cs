578f959d206f1cb384cee88df125079a243c799c
Ravi Vidap <ravividap@ymail.com>
2025-04-06 12:54:35 +0530

    replconf getack debug

--- src/ReplConfigCommand.cs
+++ src/ReplConfigCommand.cs
@@ -7,6 +7,8 @@ namespace codecrafters_redis.src
     {
         public async Task ExecuteAsync(Socket client, string[] commandParts)
         {
+            await client.SendAsync(Encoding.UTF8.GetBytes("+OK\r\n"), SocketFlags.None);
+
             string getack = commandParts[4];
 
             Console.WriteLine(getack);
