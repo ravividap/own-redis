b233af2ed11463f9a5d9c9c1d625653f2f2cc5ce
Ravi Vidap <ravividap@ymail.com>
2025-04-08 19:29:31 +0530

    reply to getack with actual offset 10

--- src/PingCommand.cs
+++ src/PingCommand.cs
@@ -7,21 +7,14 @@ namespace codecrafters_redis.src
     {
         public async Task ExecuteAsync(Socket client, string[] commandParts)
         {
-            dataStore.SetOffSet(14);
-
-            if (!isSlave)
+            if (isSlave)
+            {
+                dataStore.SetOffSet(14);
+            }
+            else
             {
                 await client.SendAsync(Encoding.UTF8.GetBytes("+PONG\r\n"), SocketFlags.None);
             }
-
-            //if (isSlave) 
-            //{
-            //    dataStore.SetOffSet(14);
-            //}
-            //else
-            //{
-            //    await client.SendAsync(Encoding.UTF8.GetBytes("+PONG\r\n"), SocketFlags.None);
-            //}
         }
     }
 }
